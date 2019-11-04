2828b84f473008adc3c32a1790b2dab9d51b9274
Jeff Hajewski <jeff.hajewski@gmail.com>
2019-11-04 10:16:37 -0600

    Reformatting

--- src/test/kotlin/com/goose/file/LoaderTest.kt
+++ src/test/kotlin/com/goose/file/LoaderTest.kt
@@ -21,18 +21,18 @@ class LoaderTest {
 		val content = "This is a Test; This will - be parsed! Don't and won't?"
 		tempFile.writeText(content)
 
-		val loader = Loader(DefaultProcessor())
-		val got = loader.loadFile(tempFile)
-		val expected = mapOf("this" to 2,
-					   "is" to 1,
-					   "a" to 1,
-					   "test" to 1,
-					   "will" to 1,
-					   "be" to 1,
-					   "parsed" to 1,
-					   "dont" to 1,
-					   "and" to 1,
-					   "wont" to 1)
-		assertEquals("frequencies are different", expected, got)
-	}
+	    val loader = Loader(DefaultProcessor())
+        val got = loader.loadFile(tempFile)
+        val expected = mapOf("this" to 2,
+                       "is" to 1,
+                       "a" to 1,
+                       "test" to 1,
+                       "will" to 1,
+                       "be" to 1,
+                       "parsed" to 1,
+                       "dont" to 1,
+                       "and" to 1,
+                       "wont" to 1)
+        assertEquals("frequencies are different", expected, got)
+    }
 }
